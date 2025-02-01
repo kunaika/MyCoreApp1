d180b4d0ce18d58f6363e503d12ac907178ab794
Kunai <kchyngyz@my.centennialcollege.ca>
2025-01-31 20:22:12 -0500

    Add project files.

---
+++ MyCoreApp1/Pages/Privacy.cshtml.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Globalization;
+
+namespace MyCoreApp1.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            // Correct Date Formatting
+            string dateTime = DateTime.Now.ToString("MM/dd/yyyy", CultureInfo.InvariantCulture);
+
+            // Assign to ViewData
+            ViewData["TimeStamp"] = dateTime;
+        }
+    }
+
+}
